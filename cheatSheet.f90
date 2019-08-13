60b6362eb1d9f790c3a356f6355fa601714b102f
Victoria Fletes <vfletes@cusd.me>
2019-08-13 11:20:12 -0700

    Create cheatSheet.f90

---
+++ cheatSheet.f90
@@ -0,0 +1,5 @@
+c this is a comment
+c using this website: https://www.fortrantutorial.com/
+c this is the library: http://www.silverfrost.com/ftn95-help/ftn95/idh_subroutines.aspx
+c SilverFrost compiler is suggested and so is PLATO IDE
+
